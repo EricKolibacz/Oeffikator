e7ac271f968aea03f377ea71f3c363f91cea09dd
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-02-15 14:46:59 +0100

    Sepeerate TimeTransformer class from notebook - into individual file

---
+++ TimeTransformer.py
@@ -0,0 +1,21 @@
+import datetime
+
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class TimeTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, start_time="12000"):
+        self.start_time = datetime.datetime.strptime("120000", "%H%M%S")
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X.loc[:, :] = X[X["Time"] != "error"]
+        X["Time"] = pd.to_datetime(X["Time"], format="%H%M%S")
+        X["Time"] = X["Time"] - self.start_time
+        X["Time"] = X["Time"].dt.total_seconds() / 60
+        X = X.dropna()
+        X = X[X["Time"] > 0]  # drop all items where time is negative
+        return X
