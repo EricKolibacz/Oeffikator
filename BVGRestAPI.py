0e4c08ca6a939279d1a250baa00393a8e3d6c948
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-02-15 14:43:09 +0100

    Include enhanced data output - instead of full json response - only dictionary with important information

--- BVGRestAPI.py
+++ BVGRestAPI.py
@@ -1,4 +1,5 @@
 import datetime
+from concurrent.futures import process
 
 import requests
 
@@ -34,4 +35,25 @@ class BVGRestAPI(APIInterface):
             ("results", str(amount_of_results)),
             ("stopovers", "true"),
         )
-        return requests.get("https://v5.bvg.transport.rest/journeys", params=params).json()
+        response = requests.get("https://v5.bvg.transport.rest/journeys", params=params).json()
+        journey = self.__process_response(response)
+        journey["origin"] = {"longitude": origin["longitude"], "latitude": origin["latitude"]}
+        return journey
+
+    def __process_response(self, response):
+        stopsovers = []
+        for leg in response["journeys"][0]["legs"]:
+            if "stopovers" in leg:
+                for stop in leg["stopovers"]:
+                    if stop["arrival"] != None:
+                        x = stop["stop"]["location"]["longitude"]
+                        y = stop["stop"]["location"]["latitude"]
+                        atime = stop["arrival"][11:19].replace(":", "")
+                        stopsovers.append({"longitude": x, "latitude": y, "time": atime})
+        destination = response["journeys"][0]["legs"][-1]
+        aTime = destination["arrival"][11:19].replace(":", "")
+        destination = {
+            "longitude": destination["destination"]["longitude"],
+            "latitude": destination["destination"]["latitude"],
+        }
+        return {"origin": None, "destination": destination, "arrivalTime": aTime, "stopovers": stopsovers}
