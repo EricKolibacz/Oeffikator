56556d596968e32cc3c3d6317ed2284baa9e1aa2
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-21 17:01:41 +0100

    Adapt abstract methods to resolve W0221
    - W0221: dismatch of number of parameters

--- apis/APIInterface.py
+++ apis/APIInterface.py
@@ -14,11 +14,13 @@ class APIInterface(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def query_location(query) -> dict:
+    def query_location(
+        self, query: str, amount_of_results=1, has_addresses="true", has_stops="false", has_poi="false"
+    ) -> dict:
         pass
 
     @abstractmethod
-    def get_journey(self) -> dict:
+    def get_journey(self, origin: dict, destination: dict, start_date: datetime, amount_of_results=1) -> dict:
         pass
 
     def has_reached_request_limit(self) -> bool:
