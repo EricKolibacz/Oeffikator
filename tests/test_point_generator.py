a9b447848c8a582369aad6b5e3d5eaaa8363094f
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-22 16:17:36 +0100

    ✅ Create test to check corner points of grid

--- tests/test_point_generator.py
+++ tests/test_point_generator.py
@@ -8,3 +8,13 @@ def test_first_point_from_grid_point_generation():
     assert first_point == [[0, 2.5]]
 
 
+def test_corner_points_from_grid_point_generation():
+    bounding_box = (0, 1, 2.5, 3.5)  # ("east", "west", "south", "north")
+    grid_point_generation = GridPointGenerator(bounding_box)
+    points = grid_point_generation.get_next_points(9)
+    assert points[0] == [0, 2.5]
+    assert points[2] == [0, 3.5]
+    assert points[6] == [1, 2.5]
+    assert points[8] == [1, 3.5]
+
+
