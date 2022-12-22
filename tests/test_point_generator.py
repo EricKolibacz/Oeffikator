c56fd59d2245fda926fd5ea6e180a7d94b87cc20
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-22 16:18:31 +0100

    🧑‍💻 Improve readability with constant
    - bounding box should be defined just once

--- tests/test_point_generator.py
+++ tests/test_point_generator.py
@@ -1,20 +1,18 @@
 from oeffikator.point_generator.GridPointGenerator import GridPointGenerator
 
+BOUNDING_BOX = (0, 1, 2.5, 3.5)  # ("east", "west", "south", "north")
+
 
 def test_first_point_from_grid_point_generation():
-    bounding_box = (0, 1, 2.5, 3.5)  # ("east", "west", "south", "north")
-    grid_point_generation = GridPointGenerator(bounding_box)
+    grid_point_generation = GridPointGenerator(BOUNDING_BOX)
     first_point = grid_point_generation.get_next_points(1)
     assert first_point == [[0, 2.5]]
 
 
 def test_corner_points_from_grid_point_generation():
-    bounding_box = (0, 1, 2.5, 3.5)  # ("east", "west", "south", "north")
-    grid_point_generation = GridPointGenerator(bounding_box)
+    grid_point_generation = GridPointGenerator(BOUNDING_BOX)
     points = grid_point_generation.get_next_points(9)
     assert points[0] == [0, 2.5]
     assert points[2] == [0, 3.5]
     assert points[6] == [1, 2.5]
     assert points[8] == [1, 3.5]
-
-
