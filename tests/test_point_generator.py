da070fdfcb2b2ca3e94867daf9d1d2be21977877
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-22 16:14:50 +0100

    🧑‍💻 Improve test case naming

--- tests/test_point_generator.py
+++ tests/test_point_generator.py
@@ -1,8 +1,10 @@
 from oeffikator.point_generator.GridPointGenerator import GridPointGenerator
 
 
-def test_grid_point_generation():
+def test_first_point_from_grid_point_generation():
     bounding_box = (0, 1, 2.5, 3.5)  # ("east", "west", "south", "north")
     grid_point_generation = GridPointGenerator(bounding_box)
     first_point = grid_point_generation.get_next_points(1)
     assert first_point == [[0, 2.5]]
+
+
