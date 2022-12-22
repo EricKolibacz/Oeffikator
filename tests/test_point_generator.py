d02d810421eb7ac38b136a225d7dc58dfcfd4005
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-22 16:21:16 +0100

    ✅ Create test to test all points of a grid
    - for now just 9 since the GridPointGenerator only generates 9 points

--- tests/test_point_generator.py
+++ tests/test_point_generator.py
@@ -16,3 +16,17 @@ def test_corner_points_from_grid_point_generation():
     assert points[2] == [0, 3.5]
     assert points[6] == [1, 2.5]
     assert points[8] == [1, 3.5]
+
+
+def test_all_points_from_grid_point_generation():
+    grid_point_generation = GridPointGenerator(BOUNDING_BOX)
+    points = grid_point_generation.get_next_points(9)
+    assert points[0] == [0, 2.5]
+    assert points[1] == [0, 3.0]
+    assert points[2] == [0, 3.5]
+    assert points[3] == [0.5, 2.5]
+    assert points[4] == [0.5, 3.0]
+    assert points[5] == [0.5, 3.5]
+    assert points[6] == [1, 2.5]
+    assert points[7] == [1, 3.0]
+    assert points[8] == [1, 3.5]
