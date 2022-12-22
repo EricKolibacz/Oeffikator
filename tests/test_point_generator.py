dc3b0fce6fa439f036891463b6e2295b4d364615
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-22 16:37:40 +0100

    ✅ Add test for first triangular point generation

--- tests/test_point_generator.py
+++ tests/test_point_generator.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 from oeffikator.point_generator.GridPointGenerator import GridPointGenerator
+from oeffikator.point_generator.TriangularPointGenerator import TriangularPointGenerator
 
 BOUNDING_BOX = (0, 1, 2.5, 3.5)  # ("east", "west", "south", "north")
 
@@ -31,3 +34,16 @@ def test_all_points_from_grid_point_generation():
     assert points[6] == [1, 2.5]
     assert points[7] == [1, 3.0]
     assert points[8] == [1, 3.5]
+
+
+# Test on TriangularPointGenerator
+
+STARTING_POINTS = np.array([[0, 0], [0, 1], [1, 1]])
+
+
+def test_first_point_from_triangular_point_generation():
+    # also test if the compute center works properly
+    point_should_be = np.mean(STARTING_POINTS, 0)
+    grid_point_generation = TriangularPointGenerator()
+    point_is = grid_point_generation.get_next_points(1, STARTING_POINTS)[0]
+    np.testing.assert_array_equal(point_is, point_should_be)
