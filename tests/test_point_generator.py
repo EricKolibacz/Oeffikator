3906a4c965471bd4e2d69f26a7ccc95ef2c36c9c
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-23 10:30:47 +0100

    🧪 Adapt points tests to iterator structure

--- tests/test_point_generator.py
+++ tests/test_point_generator.py
@@ -9,13 +9,13 @@ BOUNDING_BOX = (0, 1, 2.5, 3.5)  # ("east", "west", "south", "north")
 # Test on GridPointGenerator
 def test_first_point_from_grid_point_generation():
     grid_point_generation = GridPointGenerator(BOUNDING_BOX)
-    first_point = grid_point_generation.get_next_point()
-    assert first_point == [[0, 2.5]]
+    first_point = next(grid_point_generation)
+    assert first_point == [0, 2.5]
 
 
 def test_corner_points_from_grid_point_generation():
     grid_point_generation = GridPointGenerator(BOUNDING_BOX)
-    points = grid_point_generation.get_next_points(9)
+    points = [point for point in grid_point_generation]
     assert points[0] == [0, 2.5]
     assert points[2] == [0, 3.5]
     assert points[6] == [1, 2.5]
@@ -24,7 +24,7 @@ def test_corner_points_from_grid_point_generation():
 
 def test_all_points_from_grid_point_generation():
     grid_point_generation = GridPointGenerator(BOUNDING_BOX)
-    points = grid_point_generation.get_next_points(9)
+    points = [point for point in grid_point_generation]
     assert points[0] == [0, 2.5]
     assert points[1] == [0, 3.0]
     assert points[2] == [0, 3.5]
@@ -44,26 +44,25 @@ STARTING_POINTS = np.array([[0, 0], [0, 1], [1, 1]])
 def test_first_point_from_triangular_point_generation():
     # also test if the compute center works properly
     point_should_be = [np.mean(STARTING_POINTS, 0)]
-    grid_point_generation = TriangularPointGenerator()
-    point_is = grid_point_generation.get_next_point(STARTING_POINTS)
+    grid_point_generation = TriangularPointGenerator(STARTING_POINTS)
+    point_is = next(grid_point_generation)
     np.testing.assert_array_equal(point_is, point_should_be)
 
 
 def test_point_for_two_triangles_from_triangular_point_generation():
     point_should_be = [np.mean(STARTING_POINTS, 0)]
     new_points = np.append(STARTING_POINTS, [[1.1, 1]], axis=0)
-    grid_point_generation = TriangularPointGenerator()
-    point_is = grid_point_generation.get_next_point(new_points)
+    grid_point_generation = TriangularPointGenerator(new_points)
+    point_is = next(grid_point_generation)
     np.testing.assert_array_equal(point_is, point_should_be)
 
 
 def test_two_consective_points_from_triangular_point_generation():
     point_should_be = [np.mean(np.append(STARTING_POINTS[1:3, :], [[1 / 3, 2 / 3]], axis=0), 0)]
     new_points = np.append(STARTING_POINTS, [[1.1, 1]], axis=0)
-    grid_point_generation = TriangularPointGenerator()
-    point_is = grid_point_generation.get_next_point(STARTING_POINTS)
-    new_points = np.append(STARTING_POINTS, point_is, axis=0)
-    point_is = grid_point_generation.get_next_point(new_points)
+    grid_point_generation = TriangularPointGenerator(new_points)
+    next(grid_point_generation)
+    point_is = next(grid_point_generation)
     np.testing.assert_array_equal(point_is, point_should_be)
 
 
@@ -73,6 +72,6 @@ def test_two_points_from_triangular_point_generation():
     point2_should_be = np.mean(np.append(STARTING_POINTS[0:3:2, :], [extra_point], axis=0), 0)
     points_should_be = [point2_should_be, point1_should_be]
     new_points = np.append(STARTING_POINTS, [extra_point], axis=0)
-    grid_point_generation = TriangularPointGenerator()
-    points_are = grid_point_generation.get_next_points(2, new_points)
+    grid_point_generation = TriangularPointGenerator(new_points)
+    points_are = [next(grid_point_generation) for _ in range(2)]
     np.testing.assert_almost_equal(points_are, points_should_be)
