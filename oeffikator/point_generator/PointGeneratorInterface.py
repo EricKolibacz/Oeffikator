9295ec5ad26000b0433442424c59bf08a4edf8c7
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-23 10:11:40 +0100

    🎨 Make pointS generation generator specific
    - not generic

--- oeffikator/point_generator/PointGeneratorInterface.py
+++ oeffikator/point_generator/PointGeneratorInterface.py
@@ -6,5 +6,6 @@ class PointGeneratorInterface(ABC):
     def get_next_point(self, *args) -> dict:
         pass
 
-    def get_next_points(self, group_size, *args) -> dict:
-        return [self.get_next_point(args) for _ in range(group_size)]
+    @abstractmethod
+    def get_next_points(self, group_size: int, *args) -> dict:
+        pass
