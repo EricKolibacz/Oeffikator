2865110d37755f2c4b4a34d15f118d3a68758654
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2022-12-23 10:17:56 +0100

    🧪 Adapt GridPointGen.-test to return one point

--- tests/test_point_generator.py
+++ tests/test_point_generator.py
@@ -9,7 +9,7 @@ BOUNDING_BOX = (0, 1, 2.5, 3.5)  # ("east", "west", "south", "north")
 # Test on GridPointGenerator
 def test_first_point_from_grid_point_generation():
     grid_point_generation = GridPointGenerator(BOUNDING_BOX)
-    first_point = grid_point_generation.get_next_points(1)
+    first_point = grid_point_generation.get_next_point()
     assert first_point == [[0, 2.5]]
 
 
