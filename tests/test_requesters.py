de7f8d49f6c7f24d3de5e115b22c0a63c9a0ac3c
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-01-02 15:04:02 +0100

    🧪 Test if auth key is None

--- tests/test_requesters.py
+++ tests/test_requesters.py
@@ -9,3 +9,10 @@ def test_for_empty_auth_key():
     authkey = ""
     with pytest.raises(ValueError):
         OeffiRequester(authkey)
+
+
+def test_for_none_auth_key():
+    """Check if we receive an appropriate warning if the authkey is None."""
+    authkey = None
+    with pytest.raises(ValueError):
+        OeffiRequester(authkey)
