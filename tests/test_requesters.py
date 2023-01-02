0452a32e537995317054159ac384050f4f275ba0
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-01-02 15:44:32 +0100

    ✅ Add test for location query on bvg requester

--- tests/test_requesters.py
+++ tests/test_requesters.py
@@ -1,9 +1,22 @@
 """Tests related to the requesters."""
+import numpy as np
 import pytest
 
+from oeffikator.requesters.bvg_rest_requester import BVGRestRequester
 from oeffikator.requesters.oeffi_requester import OeffiRequester
 
 
+# BVG Rest requester
+def test_query_location():
+    """Tests if the bvg rest requester queries the location properly"""
+    coordinates_should_be = np.array([52.51627344417692, 13.37766793796735])
+    requester = BVGRestRequester()
+    location = requester.query_location("Brandenburger Tor")
+    coordinates_is = np.array([location["latitude"], location["longitude"]])
+    np.testing.assert_array_almost_equal(coordinates_should_be, coordinates_is, decimal=3)
+
+
+# Oeffi requester
 def test_for_empty_auth_key():
     """Check if we receive an appropriate warning if the authkey is empty."""
     authkey = ""
