e23e1df1356b447c58628798eeeeaea491709924
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-01-24 09:55:28 +0100

    🔊 Introduce logger

--- oeffikator/__init__.py
+++ oeffikator/__init__.py
@@ -1,4 +1,8 @@
 """App wide parameters"""
+import logging
 from importlib.metadata import version
 
 __version__ = version("oeffikator")
+
+logger = logging.getLogger("uvicorn")
+logger.propagate = 0
