77102400dd44ad1f6deea3631b891b1e05543489
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-01-24 10:19:07 +0100

    🗃️ Improve database by adding address column

--- scripts/initial_db_queries.sql
+++ scripts/initial_db_queries.sql
@@ -8,17 +8,18 @@ CREATE TABLE usage.requests(
 );
 CREATE TABLE geo.locations(
     id SERIAL,
-    geom GEOMETRY(POINT, 4326) NOT NULL,
     request_id INT,
+    address TEXT,
+    geom GEOMETRY(POINT, 4326) NOT NULL,
     CONSTRAINT requrest_id FOREIGN KEY(request_id) REFERENCES usage.requests(id),
     CONSTRAINT locations_pkey PRIMARY KEY (id)
 );
 CREATE TABLE geo.trips(
     id SERIAL,
     duration int NOT NULL,
-    request_id INT,
     origin_id INT,
     destination_id INT,
+    request_id INT,
     CONSTRAINT request_id FOREIGN KEY(request_id) REFERENCES usage.requests(id),
     CONSTRAINT origin_id FOREIGN KEY(origin_id) REFERENCES geo.locations(id),
     CONSTRAINT destination_id FOREIGN KEY(destination_id) REFERENCES geo.locations(id),
