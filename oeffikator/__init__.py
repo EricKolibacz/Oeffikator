5aec50ebc6b37c652b5ecd0c5c07504b9ffed68b
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-01-24 14:11:18 +0100

    🤡 ✨ Introduce query trips api (not working)
    - requires implementation
    - defines the tentative structure

--- oeffikator/__init__.py
+++ oeffikator/__init__.py
@@ -2,7 +2,18 @@
 import logging
 from importlib.metadata import version
 
+from .requesters.bvg_rest_requester import BVGRestRequester
+from .requesters.oeffi_requester import OeffiRequester
+
 __version__ = version("oeffikator")
 
 logger = logging.getLogger("uvicorn")
 logger.propagate = 0
+
+requester1 = BVGRestRequester()
+requesters = [requester1]
+
+AUTHKEY = ""
+if AUTHKEY != "":
+    requester2 = OeffiRequester(AUTHKEY)
+    requesters.append(requester2)
--- oeffikator/main.py
+++ oeffikator/main.py
@@ -1,7 +1,9 @@
 """Main module of the oeffikator app, providing the actual FastAPI / Uvicorn app."""
 from fastapi import FastAPI, Response
 
-from . import __version__
+from oeffikator.db_functionality import get_location, query_trips
+
+from . import __version__, logger
 
 # create App
 app = FastAPI(
@@ -18,3 +20,23 @@ async def get_service_status() -> Response:
         Response: the response to the alive statement with current version
     """
     return {"status": "ok", "version": __version__}
+
+
+@app.get("/get_trip_durations/")
+async def get_trip(origin_description: str) -> Response:
+    """Get trip durations from certain origin
+
+    Args:
+        origin_description (str): description of the trip's starting point
+
+    Returns:
+        Response: trips information (origin, destinations and durations)
+    """
+    # TODO raise error if origin_description is empty
+    logger.info("Using origin with following description: %s", origin_description)
+    origin = get_location(origin_description)
+    logger.info("We are going to use following location:")
+    logger.info("- address: %s", {origin["address"]})
+    logger.info("- coordiantes: (%s, %s)", {origin["latitude"]}, {origin["longitude"]})
+    trips = query_trips(origin["id"])
+    return {"origin": origin, "trips": trips}
