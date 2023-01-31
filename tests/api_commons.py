f120af9a43850a39479a63677ac9bd20c8fa7de9
Eric Kolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-01-31 16:59:52 +0100

    💡 Add missing docstrings

--- tests/api_commons.py
+++ tests/api_commons.py
@@ -1,9 +1,18 @@
+"""Common functionality for the api tests"""
 import requests
+from requests import Response
 
 
 class AppTestClient:
+    """A class to test the app client"""
+
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def alive(self):
+    def alive(self) -> Response:
+        """Check if the client is alive
+
+        Returns:
+            Response: requests response if the client is alive
+        """
         return requests.get(f"{self.base_url}/alive", timeout=5)
